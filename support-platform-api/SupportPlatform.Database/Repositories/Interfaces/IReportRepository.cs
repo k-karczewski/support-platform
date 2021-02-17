011c1473536ed7568c571ba358ec6949444f2324
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 09:58:39 +0100

    Interfaces for ReportRepository and RepositoryBase

---
+++ support-platform-api/SupportPlatform.Database/Repositories/Interfaces/IReportRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupportPlatform.Database
+{
+    public interface IReportRepository : IRepository<ReportEntity>
+    {
+        Task<ReportEntity> GetReportById(int id);
+        Task<ICollection<ReportEntity>> GetAllReports();
+    }
+}
