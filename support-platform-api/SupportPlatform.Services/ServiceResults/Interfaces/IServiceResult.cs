e4a5636ebd8032294d2e70c7dc7263101904bd56
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 14:50:41 +0100

    First version of ServiceResults

---
+++ support-platform-api/SupportPlatform.Services/ServiceResults/Interfaces/IServiceResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SupportPlatform.Services
+{
+    public interface IServiceResult
+    {
+        ResultType Result { get; set; }
+        ICollection<string> Errors { get; set; }
+    }
+
+    public interface IServiceResult<ReturnType> : IServiceResult
+    {
+        ReturnType ReturnedObject { get; set; }
+    }
+}
