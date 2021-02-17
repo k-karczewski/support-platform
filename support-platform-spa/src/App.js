1ac43341677eb03ce153760465163182df04256e
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 15:24:09 +0100

    Draft of Registration view

--- support-platform-spa/src/App.js
+++ support-platform-spa/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Registration from './Views/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <Registration />
     </div>
   );
 }
