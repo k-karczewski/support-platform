f61515fc2dfaf2172d1cef7d0e0cd341185932b9
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 14:51:05 +0100

    First version of UserToRegisterDto

---
+++ support-platform-api/SupportPlatform.Services/Dtos/UserToRegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class UserToRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email{ get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and Confirm password fields have to have the same value")]
+        public string ConfirmPassword { get; set; }
+    }
+}
