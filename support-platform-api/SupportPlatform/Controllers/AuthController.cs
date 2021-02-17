f346a84e85d9c3b6cfcfd2736e98368c4e5dbf63
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 14:55:00 +0100

    Draft of AuthController

---
+++ support-platform-api/SupportPlatform/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupportPlatform.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/auth/")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterAsync(UserToRegisterDto userToRegister)
+        {
+            if(ModelState.IsValid)
+            {
+                IServiceResult registrationResult = await _authService.RegisterAsync(userToRegister, Url);
+
+                if(registrationResult.Result == ResultType.Correct)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest(registrationResult.Errors);
+                }
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmailAsync(int userId, string token)
+        {
+            return Ok();
+        }
+    }
+}
