474e71c2e254fab70ea2d8d539eef5f9f9651ec8
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 14:52:25 +0100

    First version of EmailSender

---
+++ support-platform-api/SupportPlatform.Services/Services/EmailSender/Interfaces/IEmailSender.cs
@@ -0,0 +1,18 @@
+﻿using SupportPlatform.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportPlatform.Services
+{
+    public interface IEmailSender : IDisposable
+    {
+        /// <summary>
+        /// Sends confirmation email to users email address
+        /// </summary>
+        /// <param name="receiver">Recipent of email</param>
+        /// <param name="confirmationLink">confirmation link that will be included to message</param>
+        Task SendAccountConfirmation(UserEntity receiver, string confirmationLink);
+    }
+}
