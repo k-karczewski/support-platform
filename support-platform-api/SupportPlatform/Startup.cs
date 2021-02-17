fca115b8d02036b9acac39308d0de061ce6bc69a
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-17 09:41:46 +0100

    Added DbContext to service collection

--- support-platform-api/SupportPlatform/Startup.cs
+++ support-platform-api/SupportPlatform/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SupportPlatform.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,10 +28,11 @@ namespace SupportPlatform
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SupportPlatformDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +49,9 @@ namespace SupportPlatform
             {
                 endpoints.MapControllers();
             });
+
+            var db = serviceProvider.GetService<SupportPlatformDbContext>();
+            db.Database.Migrate();
         }
     }
 }
