12473c2ffe2f49b5bf4430cd9fdf1cc61657303f
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-18 15:28:50 +0100

    First version of UserToLoginDto

---
+++ support-platform-api/SupportPlatform.Services/Dtos/UserToLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupportPlatform.Services
+{
+    public class UserToLoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
