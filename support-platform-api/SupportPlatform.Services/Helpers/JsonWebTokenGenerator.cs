ffa53663f842a45cc854d9a8bd13dce8fceb232a
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-18 15:29:13 +0100

    First version of JWT token generator

---
+++ support-platform-api/SupportPlatform.Services/Helpers/JsonWebTokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SupportPlatform.Database;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public static class JsonWebTokenGenerator
+    {
+        /// <summary>
+        /// Generates Json Web Token for authorized user
+        /// </summary>
+        /// <param name="user">User object that just logged in</param>
+        /// <param name="roles">roles of the user taken from db</param>
+        /// <returns>Generated JWT</returns>
+        /*https://jasonwatmore.com/post/2019/10/11/aspnet-core-3-jwt-authentication-tutorial-with-example-api*/
+        public static string GenerateJsonWebTokenForUser(UserEntity user, ICollection<string> roles, IConfiguration configuration)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(configuration.GetSection("AuthenticationKeys:DefaultKey").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            if (roles != null && roles.Count > 0)
+            {
+                foreach(string role in roles)
+                {
+                    tokenDescriptor.Subject.AddClaim(new Claim(ClaimTypes.Role, role));
+                }
+            }
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
