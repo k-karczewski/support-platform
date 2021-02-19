7ddf3677b7b077f9fa070c86a7926f399e97625c
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-19 13:20:37 +0100

    Added userLoggedIn state initialization

--- support-platform-spa/src/App.js
+++ support-platform-spa/src/App.js
@@ -1,16 +1,27 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import {useSelector} from 'react-redux';
+
 import Navigation from './layout/navigation/Navigation';
 import RouterView from './layout/router view/RouterView';
 
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useEffect } from 'react';
+import AuthService from './_services/AuthService';
 
 function App() {
+  const userLoggedIn = useSelector(store => store.authStates.decodedToken) ? true : false;
+
+  useEffect(() => {
+    const authService = new AuthService();
+    authService.initializeStore();
+  },[])
+
   return (
     <div className="App">
-      <Router>
-        <Navigation />
-        <RouterView />
-      </Router>
+        <Router>
+          <Navigation userLoggedIn={userLoggedIn} />
+          <RouterView />
+        </Router>
     </div>
   );
 }
