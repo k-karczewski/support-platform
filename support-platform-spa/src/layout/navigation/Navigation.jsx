8368d0c9341106af853d953f9a084bb1e1bfae33
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-19 13:21:25 +0100

    Conditional rendering based on userLoggedIn state

--- support-platform-spa/src/layout/navigation/Navigation.jsx
+++ support-platform-spa/src/layout/navigation/Navigation.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthService from '../../_services/AuthService';
 
 import './Navigation.sass';
 
-const Navigation = () => {
+const Navigation = ({ userLoggedIn }) => {
+  const handleLogout = () => {
+    const authService = new AuthService();
+    authService.logout();
+  }
+
   return (
     <nav className="nav">
       <div className="nav__logo">
         <NavLink to="/">Support Platform</NavLink>
       </div>
       <ul className="nav__links">
-        <li className="nav__link">
-          <NavLink to="/login">Zaloguj się</NavLink>
-        </li>
-        <li className="nav__link">
-          <NavLink to="/register">Zarejestruj się</NavLink>
-        </li>
+        {!userLoggedIn ?
+          <>
+            <li className="nav__link">
+              <NavLink to="/login">Zaloguj się</NavLink>
+            </li>
+            <li className="nav__link">
+              <NavLink to="/register">Zarejestruj się</NavLink>
+            </li>
+          </> :
+          <li className="nav__link">
+            <NavLink to="/" onClick={handleLogout}>Wyloguj się</NavLink>
+          </li>
+        }
       </ul>
     </nav>
   );
