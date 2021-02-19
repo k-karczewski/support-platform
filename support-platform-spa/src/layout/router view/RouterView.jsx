5bba057351c6652ad7a9a481fc39f657e5ac7b66
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-19 13:21:58 +0100

    Used route guards

--- support-platform-spa/src/layout/router view/RouterView.jsx
+++ support-platform-spa/src/layout/router view/RouterView.jsx
@@ -7,16 +7,29 @@ import LoginForm from '../../views/login/login form/LoginForm';
 import AccountCreated from '../../views/register/account created/AccountCreated';
 import ConfirmationInProgress from '../../views/register/confirm messages/confirmation in progress/ConfirmationInProgress';
 import ConfirmationMessage from '../../views/register/confirm messages/confirmation message/ConfirmationMessage';
+import Dashboard from '../../views/dashboard/Dashboard';
+
+import AuthorizedRoute from '../../_guards/AuthorizedRoute';
+import UnauthorizedRoute from '../../_guards/UnauthorizedRoute';
+import AuthService from '../../_services/AuthService';
 
 const RouterView = () => {
+  const userLoggedIn = () => {
+    debugger
+    const authService = new AuthService();
+    return authService.isUserLoggedIn();
+  }
+
   return (
     <Switch>
-      {/* <Route path="/" exact component={Home} /> */}
-      <Route path="/register" component={RegisterForm} />
-      <Route path="/confirmMessage" component={ConfirmationMessage} />
-      <Route path="/confirmInProgress" component={ConfirmationInProgress} />
-      <Route path="/accountCreated" component={AccountCreated} />
-      <Route path="/login" component={LoginForm} />
+      <UnauthorizedRoute path="/login" component={LoginForm} authCondition={userLoggedIn} />
+      <UnauthorizedRoute path="/register" component={RegisterForm} authCondition={userLoggedIn} />
+      <UnauthorizedRoute path="/confirmMessage" component={ConfirmationMessage} authCondition={userLoggedIn} />
+      <UnauthorizedRoute path="/confirmInProgress" component={ConfirmationInProgress} authCondition={userLoggedIn} />
+      <UnauthorizedRoute path="/accountCreated" component={AccountCreated} authCondition={userLoggedIn} />
+
+      <AuthorizedRoute path="/" exact component={Dashboard} authCondition={userLoggedIn} />
+      <AuthorizedRoute path="/dashboard" component={Dashboard} authCondition={userLoggedIn} />
     </Switch>
   );
 }
