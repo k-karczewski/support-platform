b1824bd8f8caa91c13a5f8db6c156c5d08aa5e3d
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-19 15:21:56 +0100

    Sorted imports and removed debugger call

--- support-platform-spa/src/layout/router view/RouterView.jsx
+++ support-platform-spa/src/layout/router view/RouterView.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-import RegisterForm from '../../views/register/register form/RegisterForm';
-import LoginForm from '../../views/login/login form/LoginForm';
+import AuthorizedRoute from '../../_guards/AuthorizedRoute';
+import UnauthorizedRoute from '../../_guards/UnauthorizedRoute';
+
+import AuthService from '../../_services/AuthService';
 
 import AccountCreated from '../../views/register/account created/AccountCreated';
 import ConfirmationInProgress from '../../views/register/confirm messages/confirmation in progress/ConfirmationInProgress';
 import ConfirmationMessage from '../../views/register/confirm messages/confirmation message/ConfirmationMessage';
 import Dashboard from '../../views/dashboard/Dashboard';
+import LoginForm from '../../views/login/login form/LoginForm';
+import RegisterForm from '../../views/register/register form/RegisterForm';
 
-import AuthorizedRoute from '../../_guards/AuthorizedRoute';
-import UnauthorizedRoute from '../../_guards/UnauthorizedRoute';
-import AuthService from '../../_services/AuthService';
 
 const RouterView = () => {
   const userLoggedIn = () => {
-    debugger
     const authService = new AuthService();
     return authService.isUserLoggedIn();
   }
