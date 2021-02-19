0acc59d016b01eee69f6eab482487e3e807bbbaa
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-19 18:31:19 +0100

    Update of RouterView

--- support-platform-spa/src/layout/router view/RouterView.jsx
+++ support-platform-spa/src/layout/router view/RouterView.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
-import AuthorizedRoute from '../../_guards/AuthorizedRoute';
+import ClientRoute from '../../_guards/ClientRoute';
+import EmployeeRoute from '../../_guards/EmployeeRoute';
 import UnauthorizedRoute from '../../_guards/UnauthorizedRoute';
 
 import AuthService from '../../_services/AuthService';
@@ -15,21 +16,30 @@ import RegisterForm from '../../views/register/register form/RegisterForm';
 
 
 const RouterView = () => {
-  const userLoggedIn = () => {
+  const isUserAuthenticated = () => {
     const authService = new AuthService();
     return authService.isUserLoggedIn();
   }
 
+  const getUsersRole = () => {
+    const authService = new AuthService();
+    return authService.getDecodedToken().role;
+  }
+
   return (
     <Switch>
-      <UnauthorizedRoute path="/login" component={LoginForm} authCondition={userLoggedIn} />
-      <UnauthorizedRoute path="/register" component={RegisterForm} authCondition={userLoggedIn} />
-      <UnauthorizedRoute path="/confirmMessage" component={ConfirmationMessage} authCondition={userLoggedIn} />
-      <UnauthorizedRoute path="/confirmInProgress" component={ConfirmationInProgress} authCondition={userLoggedIn} />
-      <UnauthorizedRoute path="/accountCreated" component={AccountCreated} authCondition={userLoggedIn} />
+      <UnauthorizedRoute path="/login" component={LoginForm} authCondition={isUserAuthenticated} />
+      <UnauthorizedRoute path="/register" component={RegisterForm} authCondition={isUserAuthenticated} />
+      <UnauthorizedRoute path="/confirmMessage" component={ConfirmationMessage} authCondition={isUserAuthenticated} />
+      <UnauthorizedRoute path="/confirmInProgress" component={ConfirmationInProgress} authCondition={isUserAuthenticated} />
+      <UnauthorizedRoute path="/accountCreated" component={AccountCreated} authCondition={isUserAuthenticated} />
 
-      <AuthorizedRoute path="/" exact component={Dashboard} authCondition={userLoggedIn} />
-      <AuthorizedRoute path="/dashboard" component={Dashboard} authCondition={userLoggedIn} />
+      <ClientRoute path="/" exact component={Dashboard} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
+      <ClientRoute path="/dashboard" component={Dashboard} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
+      <ClientRoute path="/reports" component={Dashboard} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
+      <ClientRoute path="/reports/create" component={Dashboard} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
+
+      <EmployeeRoute path="/reports" component={LoginForm} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
     </Switch>
   );
 }
