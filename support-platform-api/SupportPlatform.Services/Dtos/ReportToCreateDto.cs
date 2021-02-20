da9fe39fef462ed275e2f43707d03f37f362d740
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 14:23:07 +0100

    First version of ReportToCreate dto

---
+++ support-platform-api/SupportPlatform.Services/Dtos/ReportToCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class ReportToCreateDto
+    {
+        public string Heading { get; set; }
+        public string Message { get; set; }
+        public byte[] FileInBytes { get; set; }
+    }
+}
