a1b9b1668f87c26545efba5eeb2070e518717862
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 20:21:20 +0100

    First version of CloudinaryManager

---
+++ support-platform-api/SupportPlatform.Services/Helpers/CloudinaryManager.cs
@@ -0,0 +1,44 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class CloudinaryManager : ICloudinaryManager
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryManager(IConfiguration configuration)
+        {
+            Account account = new Account
+            {
+                ApiKey = configuration.GetSection("CloudinarySettings:ApiKey").Value,
+                ApiSecret = configuration.GetSection("CloudinarySettings:ApiSecret").Value,
+                Cloud = configuration.GetSection("CloudinarySettings:CloudName").Value,
+            };
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public string UploadFile(FileToUploadDto fileToUpload, int userId)
+        {
+            Stream stream = new MemoryStream(fileToUpload.FileInBytes);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(fileToUpload.Filename, stream),
+                PublicId = Guid.NewGuid().ToString(),
+                Folder = $"reports/attachments/{userId}/",
+                Overwrite = false       
+            };
+
+            var uploadResult = _cloudinary.Upload(uploadParams);
+
+            return uploadResult.SecureUrl.AbsoluteUri;
+        }
+    }
+}
---
+++ support-platform-api/SupportPlatform.Services/Helpers/Interfaces/ICloudinaryManager.cs
@@ -0,0 +1,9 @@
+﻿using SupportPlatform.Database;
+
+namespace SupportPlatform.Services
+{
+    public interface ICloudinaryManager
+    {
+        string UploadFile(FileToUploadDto fileToUpload, int userId);
+    }
+}
