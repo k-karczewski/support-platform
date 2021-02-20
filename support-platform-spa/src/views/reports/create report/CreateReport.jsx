b5e01a6ccc769293afa9d204dfdcc2bd3b28d5fd
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 11:08:20 +0100

    First version of CreateReport view

---
+++ support-platform-spa/src/views/reports/create report/CreateReport.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CreateReportForm from '../../../components/reports/create reports/CreateReportForm';
+
+import './CreateReport.sass';
+
+const CreateReport = () => {
+  return (
+    <CreateReportForm />
+  );
+}
+
+export default CreateReport;
