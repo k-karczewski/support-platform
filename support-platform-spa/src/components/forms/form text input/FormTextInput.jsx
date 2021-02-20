36e355aca048833b71996beb88af1793d8a5a626
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 11:17:18 +0100

    Created FormTextInput component

---
+++ support-platform-spa/src/components/forms/form text input/FormTextInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './FormTextInput.sass';
+
+const FormTextInput = ({ labelText, type, htmlFor, onChangeHandler }) => {
+  return (
+    <label className="textInput__label" htmlFor={htmlFor}>
+      {labelText}
+      <input className="textInput__input" type={type} id={htmlFor} onChange={onChangeHandler} />
+    </label>
+  );
+}
+
+export default FormTextInput;
