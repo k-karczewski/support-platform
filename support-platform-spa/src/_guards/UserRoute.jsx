f75c97552c1770689d2f8049d621996de47ae499
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 11:05:48 +0100

    Created new user route

---
+++ support-platform-spa/src/_guards/UserRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const EmployeeRoute = ({ component: Component, isAuthenticated, role, ...rest }) => {
+  return (
+    <Route {...rest} render={(props) => {
+      if (isAuthenticated()) {
+        return <Component {...props} />
+      }
+      else {
+        return <Redirect to='/login' />
+      }
+    }}
+    />
+  )
+}
+
+export default EmployeeRoute
