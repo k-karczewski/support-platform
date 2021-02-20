360e26e7f6297745e7f12a659402f317e26aff6e
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-20 20:20:23 +0100

    Created Dtos for handling data of new raport

---
+++ support-platform-api/SupportPlatform.Services/Dtos/FileToUploadDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class FileToUploadDto 
+    {
+        public string Filename { get; set; }
+        public byte[] FileInBytes { get; set; }
+    }
+}
--- support-platform-api/SupportPlatform.Services/Dtos/ReportToCreateDto.cs
+++ support-platform-api/SupportPlatform.Services/Dtos/ReportToCreateDto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SupportPlatform.Services
 {
     public class ReportToCreateDto
     {
+        [Required]
         public string Heading { get; set; }
+        [Required]
         public string Message { get; set; }
-        public byte[] FileInBytes { get; set; }
+        public FileToUploadDto File { get; set; }
     }
 }
