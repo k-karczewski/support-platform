3c8ba6371ad06dbe3f61383f9350a779b500edd0
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-22 11:58:15 +0100

    Dtos for returning details of report

---
+++ support-platform-api/SupportPlatform.Services/Dtos/ModificationEntryToReturnDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class ModificationEntryToReturnDto
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ support-platform-api/SupportPlatform.Services/Dtos/ReportDetailsToReturnDto.cs
@@ -0,0 +1,19 @@
+﻿using SupportPlatform.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class ReportDetailsToReturnDto
+    {
+        public int Id { get; set; }
+        public string Heading { get; set; }
+        public string Message { get; set; }
+        public StatusEnum Status { get; set; }
+        public DateTime Date { get; set; }
+        public List<ResponseToReturnDto> Responses { get; set; }
+        public List<ModificationEntryToReturnDto> ModificationEntries { get; set; }
+        public AttachmentToReturnDto Attachment { get; set; }
+    }
+}
