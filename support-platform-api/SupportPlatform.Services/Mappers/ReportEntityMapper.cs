a0f71405b5980ef78c437ba84af899b4a389816f
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-22 11:59:53 +0100

    Mapper of ReportEntity

---
+++ support-platform-api/SupportPlatform.Services/Mappers/ReportEntityMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SupportPlatform.Database;
+
+namespace SupportPlatform.Services
+{
+    public class ReportEntityMapper
+    {
+        private IMapper _mapper;
+
+        public ReportEntityMapper()
+        {
+            _mapper = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ReportDetailsToReturnDto, ReportEntity>().ReverseMap();
+                config.CreateMap<AttachmentToReturnDto, AttachmentEntity>().ReverseMap();
+                config.CreateMap<ResponseToReturnDto, ResponseEntity>().ReverseMap();
+                config.CreateMap<ModificationEntryToReturnDto, ModificationEntryEntity>().ReverseMap();
+            }).CreateMapper();
+        }
+
+        public ReportEntity Map(ReportDetailsToReturnDto reportDetailsToReturn) => _mapper.Map<ReportEntity>(reportDetailsToReturn);
+        public ReportDetailsToReturnDto Map(ReportEntity reportEntity) => _mapper.Map<ReportDetailsToReturnDto>(reportEntity);
+    }
+}
