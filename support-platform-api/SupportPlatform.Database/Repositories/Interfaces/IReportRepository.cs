3b01922129d5a15b8f68246d6bcf5540fc32f113
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-23 20:31:27 +0100

    Divided GetAllReports method and implemented GetCount method

--- support-platform-api/SupportPlatform.Database/Repositories/Interfaces/IReportRepository.cs
+++ support-platform-api/SupportPlatform.Database/Repositories/Interfaces/IReportRepository.cs
@@ -5,7 +5,9 @@ namespace SupportPlatform.Database
 {
     public interface IReportRepository : IRepository<ReportEntity>
     {
+        int GetCount();
         Task<ReportEntity> GetReportById(int id);
-        Task<ICollection<ReportEntity>> GetAllReports();
+        Task<ICollection<ReportEntity>> GetReportsForEmployee(int numberOfPage, int itemsPerPage, int status);
+        Task<ICollection<ReportEntity>> GetReportsForClient(int numberOfPage, int itemsPerPage, int status, int userId);
     }
 }
--- support-platform-api/SupportPlatform.Database/Repositories/ReportRepository.cs
+++ support-platform-api/SupportPlatform.Database/Repositories/ReportRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,33 @@ namespace SupportPlatform.Database.Repositories
 
         public ReportRepository(SupportPlatformDbContext dbContext) : base(dbContext) { }
 
-        public async Task<ICollection<ReportEntity>> GetAllReports()
+
+        public int GetCount()
         {
-            return await _dbSet.Include(r => r.Responses).Include(m => m.ModificationEntries).Include(a => a.Attachment).ToListAsync();
+            return _dbSet.Count();
+        }
+
+        public async Task<ICollection<ReportEntity>> GetReportsForClient(int numberOfPage, int itemsPerPage, int status, int userId)
+        {
+            return await _dbSet
+                            .Where(u => u.UserId == userId)
+                            .Where(s => (int)s.Status == status)
+                            .OrderByDescending(d => d.Date.Date)
+                            .Skip(numberOfPage * itemsPerPage)
+                            .Take(itemsPerPage)
+                            .ToListAsync();
+        }
+
+        public async Task<ICollection<ReportEntity>> GetReportsForEmployee(int numberOfPage, int itemsPerPage, int status)
+        {
+            return await _dbSet
+                            .Where(s => (int)s.Status == status)
+                            .OrderByDescending(d => d.Date.Date)
+                            .ThenByDescending(d => d.Date.TimeOfDay)
+                            .Skip(numberOfPage * itemsPerPage)
+                            .Take(itemsPerPage)
+                            .Include(u => u.User)
+                            .ToListAsync();
         }
 
         public async Task<ReportEntity> GetReportById(int id)
