b3f58ae71f0930f9ddcbc8764d9d51b17dce976e
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-23 20:31:49 +0100

    Mappings for returning list of reports

--- support-platform-api/SupportPlatform.Services/Mappers/ReportEntityMapper.cs
+++ support-platform-api/SupportPlatform.Services/Mappers/ReportEntityMapper.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SupportPlatform.Database;
+using System.Collections.Generic;
 
 namespace SupportPlatform.Services
 {
@@ -17,11 +18,22 @@ namespace SupportPlatform.Services
 
                 config.CreateMap<AttachmentToReturnDto, AttachmentEntity>().ReverseMap();
                 config.CreateMap<ResponseToReturnDto, ResponseEntity>().ReverseMap().ForMember(dest => dest.Date, opt => opt.MapFrom(y => y.Date.ToString("dd/MM/yyyy HH:mm:ss")));
-                config.CreateMap<ModificationEntryToReturnDto, ModificationEntryEntity>().ReverseMap().ForMember(dest => dest.Date, opt => opt.MapFrom(y => y.Date.ToString("dd/MM/yyyy HH:mm:ss"))); ;
+                config.CreateMap<ModificationEntryToReturnDto, ModificationEntryEntity>().ReverseMap().ForMember(dest => dest.Date, opt => opt.MapFrom(y => y.Date.ToString("dd/MM/yyyy HH:mm:ss")));
+
+
+                config.CreateMap<ReportListItemToReturnDto, ReportEntity>().ReverseMap()
+                            .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(y => y.User.UserName))
+                            .ForMember(dest => dest.Date, opt => opt.MapFrom(y => y.Date.ToString("dd/MM/yyyy")))
+                            .ForMember(dest => dest.Status, opt => opt.MapFrom(y => (int)y.Status));
+
             }).CreateMapper();
         }
 
         public ReportEntity Map(ReportDetailsToReturnDto reportDetailsToReturn) => _mapper.Map<ReportEntity>(reportDetailsToReturn);
         public ReportDetailsToReturnDto Map(ReportEntity reportEntity) => _mapper.Map<ReportDetailsToReturnDto>(reportEntity);
+
+        public ICollection<ReportListItemToReturnDto> Map(ICollection<ReportEntity> reportEntities) => _mapper.Map<ICollection<ReportListItemToReturnDto>>(reportEntities);
+
+
     }
 }
