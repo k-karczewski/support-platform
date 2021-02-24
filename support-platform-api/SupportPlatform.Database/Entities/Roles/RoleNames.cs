17f25f7b367b0d0f34c046f24183f5ca186bb805
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-24 17:52:37 +0100

    Added role names class

---
+++ support-platform-api/SupportPlatform.Database/Entities/Roles/RoleNames.cs
@@ -0,0 +1,11 @@
+﻿namespace SupportPlatform.Database
+{
+    /// <summary>
+    /// Stores role names used a cross the application
+    /// </summary>
+    public class RoleNames
+    {
+        public const string Client = "Client";
+        public const string Employee = "Employee";
+    }
+}
