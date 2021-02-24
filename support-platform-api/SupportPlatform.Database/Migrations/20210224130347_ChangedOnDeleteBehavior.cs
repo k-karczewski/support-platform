fbf6f0c193942fd660d9d2c851804ddc9324434b
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-24 17:51:12 +0100

    Changed on delete behavior

---
+++ support-platform-api/SupportPlatform.Database/Migrations/20210224130347_ChangedOnDeleteBehavior.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupportPlatform.Database.Migrations
+{
+    public partial class ChangedOnDeleteBehavior : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Responses_Reports_ReportId",
+                table: "Responses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Responses_Users_UserId",
+                table: "Responses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Responses_Reports_ReportId",
+                table: "Responses",
+                column: "ReportId",
+                principalTable: "Reports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Responses_Users_UserId",
+                table: "Responses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Responses_Reports_ReportId",
+                table: "Responses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Responses_Users_UserId",
+                table: "Responses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Responses_Reports_ReportId",
+                table: "Responses",
+                column: "ReportId",
+                principalTable: "Reports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Responses_Users_UserId",
+                table: "Responses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
