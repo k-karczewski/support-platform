dd4e0bd7a7fc137e36f38316a34e1a0cc6e516ae
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-24 17:57:39 +0100

    Created parameters of report list pagination

---
+++ support-platform-api/SupportPlatform.Services/Helpers/ReportListParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportPlatform.Services
+{
+    public class ReportListParams
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize = 10;
+        public int? StatusFilter = null;
+    }
+}
