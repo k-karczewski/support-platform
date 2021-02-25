bf79dc856eb6f45889ceaeef701af7aa791f186f
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-25 20:29:29 +0100

    Improved valiadtion

--- support-platform-spa/src/components/reports/details/response form/ResponseForm.jsx
+++ support-platform-spa/src/components/reports/details/response form/ResponseForm.jsx
@@ -3,20 +3,36 @@ import { Prompt } from 'react-router-dom';
 
 import FormSubmitButton from '../../../shared/forms/submit button/FormSubmitButton';
 import FormTextAreaInput from '../../../shared/forms/textarea input/FormTextAreaInput';
+import FormErrorsPanel from '../../../shared/forms/errors panel/FormErrorsPanel';
 
 import './ResponseForm.sass';
 
 const ResponseForm = ({ sendResponseHandler }) => {
   const [response, setResponse] = useState('');
+  const [formErrors, setFormErrors] = useState([]);
 
   const handleResponse = event => {
     setResponse(event.target.value);
   }
 
+  const formIsValid = () => {
+    const formErrors = [];
+    if (response.length === 0) {
+      formErrors.push("Proszę wpisać wiadomość")
+    }
+
+    setFormErrors(formErrors);
+    return formErrors.length === 0 ? true : false;
+  }
+
   const handleSubmit = event => {
     event.preventDefault();
-    sendResponseHandler(response);
-    setResponse('');
+
+    if (formIsValid()) {
+      sendResponseHandler(response);
+      setResponse('');
+      setFormErrors([]);
+    }
   }
 
   return (
@@ -26,10 +42,12 @@ const ResponseForm = ({ sendResponseHandler }) => {
         <FormTextAreaInput htmlFor="response" onChangeHandler={handleResponse} placeholder="Wpisz wiadomość tutaj..." value={response} />
         <FormSubmitButton text="Wyślij wiadomość" />
       </form>
+      { formErrors.length > 0 ? <FormErrorsPanel errors={formErrors} /> : null}
+
       <Prompt
         when={response.length > 0}
         message="Masz niezapisane zmiany. 
-                Czy na pewno chcesz opuścić tę stronę?"
+        Czy na pewno chcesz opuścić tę stronę?"
       />
     </section>
   );
