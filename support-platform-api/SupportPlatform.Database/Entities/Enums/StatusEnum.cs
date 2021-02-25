3cafc46ef737cbbead101d460d9cdb9434fce589
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-25 14:25:43 +0100

    Changed name of enum

--- support-platform-api/SupportPlatform.Database/Entities/Enums/StatusEnum.cs
+++ support-platform-api/SupportPlatform.Database/Entities/Enums/StatusEnum.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SupportPlatform.Database
+﻿namespace SupportPlatform.Database
 {
     public enum StatusEnum
     {
         New,
-        Pending,
+        InProgress,
         Completed
     }
 }
