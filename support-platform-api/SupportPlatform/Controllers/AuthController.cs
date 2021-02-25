8dcf022a14b333b4942181168d575dacbad134eb
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-25 14:33:30 +0100

    Removed unused using statements

--- support-platform-api/SupportPlatform/Controllers/AuthController.cs
+++ support-platform-api/SupportPlatform/Controllers/AuthController.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SupportPlatform.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SupportPlatform.Controllers
