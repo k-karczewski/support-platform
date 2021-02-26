2871d601174edda025b6e522035877dd23f02b97
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-26 11:55:37 +0100

    Divided Error int Auth and Server errors

--- support-platform-spa/src/layout/route view/RouteView.jsx
+++ support-platform-spa/src/layout/route view/RouteView.jsx
@@ -7,15 +7,16 @@ import UnauthorizedRoute from '../../_route guards/UnauthorizedRoute';
 
 import AuthService from '../../_services/AuthService';
 
+import AuthError from '../../views/error/auth error/AuthError';
 import AccountCreated from '../../views/register/account created/AccountCreated';
 import ConfirmationInProgress from '../../views/register/confirm messages/confirmation in progress/ConfirmationInProgress';
 import ConfirmationMessage from '../../views/register/confirm messages/confirmation message/ConfirmationMessage';
 import CreateReport from '../../views/reports/create report/CreateReport';
-import Error from '../../views/error/Error';
 import LoginForm from '../../views/login/login form/LoginForm';
 import RegisterForm from '../../views/register/register form/RegisterForm';
 import ReportsOverview from '../../views/reports/reports overview/ReportsOverview';
 import ReportDetails from '../../views/reports/report details/ReportDetails';
+import ServerError from '../../views/error/server error/ServerError';
 
 
 const RouterView = () => {
@@ -41,12 +42,13 @@ const RouterView = () => {
         <UserRoute path="/" exact component={ReportsOverview} isAuthenticated={isUserAuthenticated} />
         <UserRoute path="/reports" exact component={ReportsOverview} isAuthenticated={isUserAuthenticated} />
         <UserRoute path="/reports/details/:id" exact component={ReportDetails} isAuthenticated={isUserAuthenticated} />
-        <UserRoute path="/error" component={Error} isAuthenticated={isUserAuthenticated} />
+        <UserRoute path="/error-auth" component={AuthError} isAuthenticated={isUserAuthenticated} />
+        <UserRoute path="/error-server" component={ServerError} isAuthenticated={isUserAuthenticated} />
 
         <ClientRoute path="/reports/create" exact component={CreateReport} isAuthenticated={isUserAuthenticated} role={getUsersRole} />
 
         {/* default route */}
-        <UserRoute component={Error} isAuthenticated={isUserAuthenticated} />
+        <UserRoute component={AuthError} isAuthenticated={isUserAuthenticated} />
       </Switch>
     </div>
   );
