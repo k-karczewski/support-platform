7bf5a3385d8af382aad5a68081c414ea7d4e90bc
Konrad Karczewski <kkarczewski94@gmail.com>
2021-02-28 09:35:21 +0100

    Added about controller (only for application start)

---
+++ support-platform-api/SupportPlatform/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupportPlatform.Controllers
+{
+    /// <summary>
+    /// This controller is not part of the application
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult About()
+        {
+            return Ok(new 
+            { 
+                Firstname = "Konrad",
+                LastName = "Karczewski",
+                Repository = "https://github.com/k-karczewski/support-platform"
+            });
+        }
+
+    }
+}
